3bfc494c1cfdeb83bc6d3e5eccb6f0574974454d
dylaron <yuliangd@gmail.com>
2021-05-28 14:35:35 -0700

    pin shifted

--- src/Defines.h
+++ src/Defines.h
@@ -1,5 +1,5 @@
 // IO pin assignments
-#define BUTTONPIN 2    // a N.O. pushbutton between this and GND. Int Pull-up resistor to be used. Start/Stop
+#define BUTTONPIN 3    // a N.O. pushbutton between this and GND. Int Pull-up resistor to be used. Start/Stop
 #define LEDRINGPIN 4   // input pin of the LED ring
 #define TONEPIN 9      // a piezo buzzer, between this and GND.
 #define PIXELOFFSET 14 // offset for led pixel to get the 1st one at top.
