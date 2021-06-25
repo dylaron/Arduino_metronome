c16c94cd68c9e290c4959596a0c90686faf6f817
dylaron <yuliangd@gmail.com>
2021-06-25 00:34:45 -0700

    pin reassigned

--- src/Defines.h
+++ src/Defines.h
@@ -1,8 +1,8 @@
 // IO pin assignments
-#define BUTTONPIN 3    // a N.O. pushbutton between this and GND. Int Pull-up resistor to be used. Start/Stop
-#define LEDRINGPIN 4   // input pin of the LED ring
+#define BUTTONPIN 6    // a N.O. pushbutton between this and GND. Int Pull-up resistor to be used. Start/Stop
+#define LEDRINGPIN 3   // input pin of the LED ring
 #define TONEPIN 9      // a piezo buzzer, between this and GND.
-#define PIXELOFFSET 14 // offset for led pixel to get the 1st one at top.
+#define PIXELOFFSET 4 // offset for led pixel to get the 1st one at top.
 
 #define NUMPIXELS 16
 // Parameters
@@ -16,4 +16,4 @@
 #define RGB1STTICK 0x00C0A000  // color for the first tick
 #define RGBRESTTICK 0x00404010 // color for the rest of ticks
 #define RGBFLASHS 0x00606060   // color for the major flash /accent
-#define RGBFLASHW 0x00202020   // color for the rest flash /accent
+#define RGBFLASHW 0x00202020   // color for the rest flash /accent 
